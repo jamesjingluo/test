93b126eb4fa94ca0d62591350c843f0ea1fec966
luojing <luojing1@xiaomi.com>
2014-10-23 17:44:16 +0800

    how are you in out

--- src/util/MD5.java
+++ src/util/MD5.java
@@ -8,6 +8,7 @@ public class MD5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello, I am Luojing");
+        System.out.println("I am very good , how are you?");
 	}
 
 }
